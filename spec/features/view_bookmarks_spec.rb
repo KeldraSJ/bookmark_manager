1e7abe52df4dfc024244362a59278af8272a2efb
Keldra <KeldraSJ@gmail.com>
2021-09-16 16:39:30 +0100

    setting up a test environment

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,7 +1,16 @@
 feature 'Viewing bookmarks' do
   scenario 'view a list of bookmarks' do
+
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.google.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.twitter.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.instagram.com');")
+
     visit('/bookmarks')
+
     expect(page).to have_content 'http://www.google.com'
     expect(page).to have_content 'http://www.twitter.com'
+    expect(page).to have_content 'http://www.instagram.com'
   end
 end
--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -3,10 +3,17 @@ require 'bookmark'
 describe 'Bookmark' do
   describe '#all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.twitter.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.instagram.com');")
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include('http://www.google.com')
       expect(bookmarks).to include('http://www.twitter.com')
+      expect(bookmarks).to include('http://www.instagram.com')
     end
   end
 end
